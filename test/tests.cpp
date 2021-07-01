14c1061beebba2e0f8fe6c2b8c0bf26a4a6d3c73
Elviss Strazdins <elviss@elviss.lv>
2021-07-01 03:27:06 +0300

    Add test for string

--- test/tests.cpp
+++ test/tests.cpp
@@ -9,3 +9,9 @@ TEST_CASE("Integer")
     const plist::Value d = 1;
     REQUIRE(d.as<std::int64_t>() == 1);
 }
+
+TEST_CASE("String")
+{
+    const plist::Value d = "test";
+    REQUIRE(d.as<std::string>() == "test");
+}
