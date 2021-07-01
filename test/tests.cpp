b426e2c3794a749aa3d843ba083df8e38cf3f5ad
Elviss Strazdins <elviss@elviss.lv>
2021-07-01 14:35:24 +0300

    Add tests for assignment

--- test/tests.cpp
+++ test/tests.cpp
@@ -6,48 +6,103 @@
 
 TEST_CASE("Bool constructor", "[constructors]")
 {
-    const plist::Value d = true;
-    REQUIRE(d.as<bool>() == true);
+    const plist::Value v = true;
+    REQUIRE(v.as<bool>() == true);
 }
 
 TEST_CASE("Integer constructor", "[constructors]")
 {
-    const plist::Value d = 1;
-    REQUIRE(d.as<std::int64_t>() == 1);
+    const plist::Value v = 1;
+    REQUIRE(v.as<std::int64_t>() == 1);
 }
 
 TEST_CASE("String constructor", "[constructors]")
 {
-    const plist::Value d = std::string{"test"};
-    REQUIRE(d.as<std::string>() == "test");
+    const plist::Value v = std::string{"test"};
+    REQUIRE(v.as<std::string>() == "test");
 }
 
 TEST_CASE("String literal constructor", "[constructors]")
 {
-    const plist::Value d = "test";
-    REQUIRE(d.as<std::string>() == "test");
+    const plist::Value v = "test";
+    REQUIRE(v.as<std::string>() == "test");
 }
 
 TEST_CASE("Array constructor", "[constructors]")
 {
-    const plist::Value d = plist::Array{plist::Value{0}, plist::Value{1}};
-    REQUIRE(d.as<plist::Array>().size() == 2);
-    REQUIRE(d[0].as<std::int64_t>() == 0);
-    REQUIRE(d[1].as<std::int64_t>() == 1);
+    const plist::Value v = plist::Array{plist::Value{0}, plist::Value{1}};
+    REQUIRE(v.as<plist::Array>().size() == 2);
+    REQUIRE(v[0].as<std::int64_t>() == 0);
+    REQUIRE(v[1].as<std::int64_t>() == 1);
 }
 
 TEST_CASE("Dictionary constructor", "[constructors]")
 {
-    const plist::Value d = plist::Dictionary{{"0", plist::Value{0}}, {"1", plist::Value{1}}};
-    REQUIRE(d.as<plist::Dictionary>().size() == 2);
-    REQUIRE(d["0"].as<std::int64_t>() == 0);
-    REQUIRE(d["1"].as<std::int64_t>() == 1);
+    const plist::Value v = plist::Dictionary{{"0", plist::Value{0}}, {"1", plist::Value{1}}};
+    REQUIRE(v.as<plist::Dictionary>().size() == 2);
+    REQUIRE(v["0"].as<std::int64_t>() == 0);
+    REQUIRE(v["1"].as<std::int64_t>() == 1);
 }
 
 TEST_CASE("Data constructor", "[constructors]")
 {
-    const plist::Value d = plist::Data{std::byte{0U}, std::byte{1U}};
-    REQUIRE(d.as<plist::Data>().size() == 2);
-    REQUIRE(d.as<plist::Data>()[0] == std::byte{0U});
-    REQUIRE(d.as<plist::Data>()[1] == std::byte{1U});
+    const plist::Value v = plist::Data{std::byte{0U}, std::byte{1U}};
+    REQUIRE(v.as<plist::Data>().size() == 2);
+    REQUIRE(v.as<plist::Data>()[0] == std::byte{0U});
+    REQUIRE(v.as<plist::Data>()[1] == std::byte{1U});
+}
+
+TEST_CASE("Bool assignment", "[assignments]")
+{
+    plist::Value v;
+    v = true;
+    REQUIRE(v.as<bool>() == true);
+}
+
+TEST_CASE("Integer assignment", "[assignments]")
+{
+    plist::Value v;
+    v = 1;
+    REQUIRE(v.as<std::int64_t>() == 1);
+}
+
+TEST_CASE("String assignment", "[assignments]")
+{
+    plist::Value v;
+    v = std::string{"test"};
+    REQUIRE(v.as<std::string>() == "test");
+}
+
+TEST_CASE("String literal assignment", "[assignments]")
+{
+    plist::Value v;
+    v = "test";
+    REQUIRE(v.as<std::string>() == "test");
+}
+
+TEST_CASE("Array assignment", "[assignments]")
+{
+    plist::Value v;
+    v = plist::Array{plist::Value{0}, plist::Value{1}};
+    REQUIRE(v.as<plist::Array>().size() == 2);
+    REQUIRE(v[0].as<std::int64_t>() == 0);
+    REQUIRE(v[1].as<std::int64_t>() == 1);
+}
+
+TEST_CASE("Dictionary assignment", "[assignments]")
+{
+    plist::Value v;
+    v = plist::Dictionary{{"0", plist::Value{0}}, {"1", plist::Value{1}}};
+    REQUIRE(v.as<plist::Dictionary>().size() == 2);
+    REQUIRE(v["0"].as<std::int64_t>() == 0);
+    REQUIRE(v["1"].as<std::int64_t>() == 1);
+}
+
+TEST_CASE("Data assignment", "[assignments]")
+{
+    plist::Value v;
+    v = plist::Data{std::byte{0U}, std::byte{1U}};
+    REQUIRE(v.as<plist::Data>().size() == 2);
+    REQUIRE(v.as<plist::Data>()[0] == std::byte{0U});
+    REQUIRE(v.as<plist::Data>()[1] == std::byte{1U});
 }
