0f496c334e44dab788bfbfaca84c0242cba94bc6
Elviss Strazdins <elviss@elviss.lv>
2021-07-04 21:13:44 +0300

    Add is checks to assignment tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -63,6 +63,7 @@ TEST_CASE("Bool assignment", "[assignments]")
 {
     plist::Value v;
     v = true;
+    REQUIRE(v.is<bool>());
     REQUIRE(v.as<bool>() == true);
 }
 
@@ -70,6 +71,7 @@ TEST_CASE("Integer assignment", "[assignments]")
 {
     plist::Value v;
     v = 1;
+    REQUIRE(v.is<std::int64_t>());
     REQUIRE(v.as<std::int64_t>() == 1);
 }
 
@@ -77,6 +79,7 @@ TEST_CASE("String assignment", "[assignments]")
 {
     plist::Value v;
     v = std::string{"test"};
+    REQUIRE(v.is<std::string>());
     REQUIRE(v.as<std::string>() == "test");
 }
 
@@ -84,6 +87,7 @@ TEST_CASE("String literal assignment", "[assignments]")
 {
     plist::Value v;
     v = "test";
+    REQUIRE(v.is<std::string>());
     REQUIRE(v.as<std::string>() == "test");
 }
 
@@ -91,6 +95,7 @@ TEST_CASE("Array assignment", "[assignments]")
 {
     plist::Value v;
     v = plist::Array{plist::Value{0}, plist::Value{1}};
+    REQUIRE(v.is<plist::Array>());
     REQUIRE(v.as<plist::Array>().size() == 2);
     REQUIRE(v[0].as<std::int64_t>() == 0);
     REQUIRE(v[1].as<std::int64_t>() == 1);
@@ -100,6 +105,7 @@ TEST_CASE("Dictionary assignment", "[assignments]")
 {
     plist::Value v;
     v = plist::Dictionary{{"0", plist::Value{0}}, {"1", plist::Value{1}}};
+    REQUIRE(v.is<plist::Dictionary>());
     REQUIRE(v.as<plist::Dictionary>().size() == 2);
     REQUIRE(v["0"].as<std::int64_t>() == 0);
     REQUIRE(v["1"].as<std::int64_t>() == 1);
@@ -109,6 +115,7 @@ TEST_CASE("Data assignment", "[assignments]")
 {
     plist::Value v;
     v = plist::Data{std::byte{0U}, std::byte{1U}};
+    REQUIRE(v.is<plist::Data>());
     REQUIRE(v.as<plist::Data>().size() == 2);
     REQUIRE(v.as<plist::Data>()[0] == std::byte{0U});
     REQUIRE(v.as<plist::Data>()[1] == std::byte{1U});
